988ed565aae49ba68e2f0fa0f414ae85c93922fa
kabalist7-pc <kabalist7@gmail.com>
2026-08-03 13:56:18 +0000

    Update history.js

--- history.js
+++ history.js
@@ -1,14 +1,48 @@
+/*
+=====================================================
+TRON ANALYZER
+Version : 1.0
+Fichier : history.js
+=====================================================
+*/
+
+"use strict";
+
 class HistoryService {
 
-    static list() {
+    static all() {
 
         return StorageService.getHistory();
 
     }
 
-    static count() {
+    static latest() {
 
-        return this.list().length;
+        const history = this.all();
+
+        if (history.length === 0) {
+
+            return null;
+
+        }
+
+        return history[0];
+
+    }
+
+    static total() {
+
+        return this.all().length;
+
+    }
+
+    static exists(nonce) {
+
+        return this.all().some(
+
+            item => item.nonce === nonce
+
+        );
 
     }
 
