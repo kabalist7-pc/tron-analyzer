01326d616ff93d772c06f8824faaf3366b102b3d
kabalist7-pc <kabalist7@gmail.com>
2026-08-04 19:12:00 +0000

    Update testbench.js

--- testbench.js
+++ testbench.js
@@ -1,1 +1,41 @@
+/*
+=========================================================
+TRON ANALYZER
+TESTBENCH V1
+Sprint 1 - Livraison 1 - Partie 2
+=========================================================
+*/
 
+"use strict";
+
+async function runCryptoTest() {
+
+    const serverSeed =
+        "48789882eae0323fb5c2576eb5b46abe24d827fe304f26c7c1aea027c004d2c6";
+
+    const clientSeed =
+        "Angegardienpc@";
+
+    const nonce =
+        "617843";
+
+    const message =
+        clientSeed + ":" + nonce;
+
+    const result =
+        await CryptoEngine.hmac(
+            message,
+            serverSeed
+        );
+
+    console.log("===== TEST CRYPTO =====");
+
+    console.log("Message :", message);
+
+    console.log("HEX :", result.hex);
+
+    console.log("BASE64 :", result.base64);
+
+    console.log("BYTES :", result.bytes);
+
+}
